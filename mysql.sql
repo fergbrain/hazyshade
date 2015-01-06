f4a5e76abffb7788b0d48b62beecb8ce3e109f45
Andrew Ferguson <andrew@fergcorp.com>
2015-01-06 13:20:03 -0800

    Create mysql.sql
    `id` should be an auto increasing number
    `singer` should be the list of sings for any given song separated by a vertical bar. Example: Andrew|Bob|Chris
    `song` should be the name of the song

---
+++ mysql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `song_to_singer` (
+'id' int(11) NOT NULL auto_increment,
+'singer' varchar(255) NOT NULL default '',
+'song' varchar(255) NOT NULL default '',
+PRIMARY KEY ('id')
+) TYPE=MyISAM AUTO_INCREMENT=36;
